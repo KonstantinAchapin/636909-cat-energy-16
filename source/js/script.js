b6c6b8a46a335c9a4c4703f9d4c80eed19d981bc
Константин Ачапин <KonstantinAchapin@mail.ru>
2019-04-15 18:00:08 +0200

    Добавляет в разметку раздел after/before + скрипт смены изображения кота (reveal), который полностью доделаю позже

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,25 @@
+//Открытие-закрытие мобильного меню
+
 var buttonOpen = document.querySelector(".page-header__nav-toggle");
 var navMenu = document.querySelector(".main-nav");
 
 navMenu.classList.remove('main-nav--nojs');
 
-  buttonOpen.addEventListener('click', function() {
-    if (navMenu.classList.contains('main-nav--closed')) {
-      navMenu.classList.remove('main-nav--closed');
-      navMenu.classList.add('main-nav--opened');
-    } else {
-      navMenu.classList.add('main-nav--closed');
-      navMenu.classList.remove('main-nav--opened');
-    }
+buttonOpen.addEventListener('click', function() {
+  if (navMenu.classList.contains('main-nav--closed')) {
+    navMenu.classList.remove('main-nav--closed');
+    navMenu.classList.add('main-nav--opened');
+  } else {
+    navMenu.classList.add('main-nav--closed');
+    navMenu.classList.remove('main-nav--opened');
+  }
 });
+
+//Смена картинки с помощью input-range
+function revealImage() {
+  size = document.querySelector(".usage-example__range").value;
+  imgFirst = document.querySelector(".usage-example__image--before");
+  imgFirst.style.clip = "rect(0px, " + (size) + "px, auto, 0px)";
+  imgSecond = document.querySelector(".usage-example__image--after");
+  imgSecond.style.clip = "rect(0px, auto, auto, " + (size) + "px)";
+}
