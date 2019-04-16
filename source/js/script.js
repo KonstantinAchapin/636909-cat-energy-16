de41935d58ac36ceff35bc7b8af4f3dccb51ff9d
Константин Ачапин <KonstantinAchapin@mail.ru>
2019-04-16 11:51:11 +0200

    Добавляет мелкие изменения в разметку и блок с картой location

--- source/js/script.js
+++ source/js/script.js
@@ -9,17 +9,61 @@ buttonOpen.addEventListener('click', function() {
   if (navMenu.classList.contains('main-nav--closed')) {
     navMenu.classList.remove('main-nav--closed');
     navMenu.classList.add('main-nav--opened');
+    buttonOpen.classList.add('page-header__nav-toggle--opened');
   } else {
     navMenu.classList.add('main-nav--closed');
     navMenu.classList.remove('main-nav--opened');
+    buttonOpen.classList.remove('page-header__nav-toggle--opened');
   }
 });
 
 //Смена картинки с помощью input-range
+
 function revealImage() {
   size = document.querySelector(".usage-example__range").value;
-  imgFirst = document.querySelector(".usage-example__image--before");
+  imgFirst = document.querySelector(".usage-example__image--after");
   imgFirst.style.clip = "rect(0px, " + (size) + "px, auto, 0px)";
-  imgSecond = document.querySelector(".usage-example__image--after");
+  imgSecond = document.querySelector(".usage-example__image--before");
   imgSecond.style.clip = "rect(0px, auto, auto, " + (size) + "px)";
 }
+
+//JS для яндекс карт
+ymaps.ready(function() {
+  var myMap = new ymaps.Map('map', {
+      center: [59.938787, 30.323164],
+      zoom: 16,
+      controls: []
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemarkWithContent = new ymaps.Placemark([59.939425, 30.322500], {
+      hintContent: '',
+      balloonContent: '',
+      iconContent: ''
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#imageWithContent',
+      // Своё изображение иконки метки.
+      iconImageHref: '../img/map-pin.png',
+      // Размеры метки.
+      iconImageSize: [62, 53],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [0, 0],
+      // Смещение слоя с содержимым относительно слоя с картинкой.
+      iconContentOffset: [0, 0],
+      // Макет содержимого.
+      iconContentLayout: MyIconContentLayout
+    });
+
+  myMap.geoObjects
+    .add(myPlacemarkWithContent);
+  myMap.behaviors.disable('scrollZoom');
+});
