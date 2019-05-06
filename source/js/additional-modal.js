30b65b4a8b4f61429df070fbf3ff626e13823c3f
Константин Ачапин <KonstantinAchapin@mail.ru>
2019-05-06 16:25:39 +0200

    Правки перед отправкой на предзащиту

---
+++ source/js/additional-modal.js
@@ -0,0 +1,13 @@
+//Псевдо открытие модального окна доп товаров
+var additionalButton = document.querySelectorAll(".additional-goods__toggle");
+
+var clickButton = function (button) {
+  button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    alert("Открылось модальное окно");
+  });
+};
+
+for (var i = 0; i <= additionalButton.length; i++) {
+  clickButton(additionalButton[i]);
+}
--- source/js/script.js
+++ source/js/script.js
@@ -59,27 +59,3 @@ ymaps.ready(function() {
     .add(myPlacemarkWithContent);
   myMap.behaviors.disable('scrollZoom');
 });
-
-//Псевдо загрузка по ajax новых товаров
-
-var catalogAddButton = document.querySelector(".catalog-add__toggle");
-
-catalogAddButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  alert("Загрузились новые товары по ajax");
-});
-
-//Псевдо открытие модального окна
-
-var catalogButton = document.querySelectorAll(".catalog-list__toggle");
-
-var clickButton = function (button) {
-  button.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    alert("Открылось модальное окно");
-  });
-};
-
-for (var i = 0; i <= catalogButton.length; i++) {
-  clickButton(catalogButton[i]);
-}
